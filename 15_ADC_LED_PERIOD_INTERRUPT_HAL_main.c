565b3f200da85a259e3864475ddfeb1ed83fdb03
maroro0220 <30305125+maroro0220@users.noreply.github.com>
2017-09-07 15:22:21 +0900

    Rename main.c to 15_ADC_LED_PERIOD_INTERRUPT_HAL_main.c

--- 15_ADC_LED_PERIOD_INTERRUPT_HAL_main.c
+++ 15_ADC_LED_PERIOD_INTERRUPT_HAL_main.c
@@ -1,5 +1,5 @@
 /*
- * This file is part of the ÂµOS++ distribution.
+ * This file is part of the Ã‚ÂµOS++ distribution.
  *   (https://github.com/micro-os-plus)
  * Copyright (c) 2014 Liviu Ionescu.
  *
@@ -52,18 +52,18 @@
 #pragma GCC diagnostic ignored "-Wmissing-declarations"
 #pragma GCC diagnostic ignored "-Wreturn-type"
 
-GPIO_InitTypeDef GPIO_Init_Struct; // GPIOÀÇ ÃÊ±âÈ­¸¦ À§ÇÑ ±¸Á¶Ã¼ÇüÀÇ º¯¼ö¸¦ ¼±¾ğ
-ADC_HandleTypeDef AdcHandler; // ADCÀÇ ÃÊ±âÈ­¸¦ À§ÇÑ ±¸Á¶Ã¼ÇüÀÇ º¯¼ö¸¦ ¼±¾ğ
+GPIO_InitTypeDef GPIO_Init_Struct; // GPIOÃ€Ã‡ ÃƒÃŠÂ±Ã¢ÃˆÂ­Â¸Â¦ Ã€Â§Ã‡Ã‘ Â±Â¸ÃÂ¶ÃƒÂ¼Ã‡Ã¼Ã€Ã‡ ÂºÂ¯Â¼Ã¶Â¸Â¦ Â¼Â±Â¾Ã°
+ADC_HandleTypeDef AdcHandler; // ADCÃ€Ã‡ ÃƒÃŠÂ±Ã¢ÃˆÂ­Â¸Â¦ Ã€Â§Ã‡Ã‘ Â±Â¸ÃÂ¶ÃƒÂ¼Ã‡Ã¼Ã€Ã‡ ÂºÂ¯Â¼Ã¶Â¸Â¦ Â¼Â±Â¾Ã°
 ADC_ChannelConfTypeDef sConfig;
-int adc_value;// ADC°ª ÀúÀå º¯¼ö
+int adc_value;// ADCÂ°Âª Ã€ÃºÃ€Ã¥ ÂºÂ¯Â¼Ã¶
 
-/*Áö¿¬·çÆ¾*/
-static void ms_delay_int_count(volatile unsigned int nTime) // ms Áö¿¬
+/*ÃÃ¶Â¿Â¬Â·Ã§Ã†Â¾*/
+static void ms_delay_int_count(volatile unsigned int nTime) // ms ÃÃ¶Â¿Â¬
 {
 nTime = (nTime * 14000);
 for(; nTime > 0; nTime--);
 }
-static void us_delay_int_count(volatile unsigned int nTime) // us Áö¿¬
+static void us_delay_int_count(volatile unsigned int nTime) // us ÃÃ¶Â¿Â¬
 {
 nTime = (nTime * 12);
 for(; nTime > 0; nTime--);
@@ -101,24 +101,24 @@ void ADC_Config(){
 //	HAL_NVIC_SetPriority(ADC_IRQn,0,0);
 //	HAL_NVIC_EnableIRQ(ADC_IRQn);
 
-	__HAL_RCC_ADC1_CLK_ENABLE(); // ADC Å¬·° ÃR¼ºÈ­
-	__HAL_RCC_GPIOA_CLK_ENABLE(); // ADC ÇÉÀ¸·Î »ç¿ëÇÒ GPIOx ÃR¼ºÈ­(VR:PA4)
-	GPIO_Init_Struct.Pin = GPIO_PIN_4; // GPIO¿¡¼­ »ç¿ëÇÒ PIN ¼³Á¤
-	GPIO_Init_Struct.Mode = GPIO_MODE_ANALOG; // Input Analog Mode ¸ğµå
+	__HAL_RCC_ADC1_CLK_ENABLE(); // ADC Ã…Â¬Â·Â° ÃƒRÂ¼ÂºÃˆÂ­
+	__HAL_RCC_GPIOA_CLK_ENABLE(); // ADC Ã‡Ã‰Ã€Â¸Â·Ã Â»Ã§Â¿Ã«Ã‡Ã’ GPIOx ÃƒRÂ¼ÂºÃˆÂ­(VR:PA4)
+	GPIO_Init_Struct.Pin = GPIO_PIN_4; // GPIOÂ¿Â¡Â¼Â­ Â»Ã§Â¿Ã«Ã‡Ã’ PIN Â¼Â³ÃÂ¤
+	GPIO_Init_Struct.Mode = GPIO_MODE_ANALOG; // Input Analog Mode Â¸Ã°ÂµÃ¥
 	HAL_GPIO_Init(GPIOA, &GPIO_Init_Struct);
-	AdcHandler.Instance = ADC1; // ADC1 ¼³Á¤
+	AdcHandler.Instance = ADC1; // ADC1 Â¼Â³ÃÂ¤
 	AdcHandler.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV2; // ADC clock prescaler
 	AdcHandler.Init.Resolution = ADC_RESOLUTION_12B; // ADC resolution
 	AdcHandler.Init.DataAlign = ADC_DATAALIGN_RIGHT; // ADC data alignment
-	AdcHandler.Init.ScanConvMode = DISABLE; // ADC scan ¸ğµå ºñÃR¼ºÈ­
-	AdcHandler.Init.ContinuousConvMode = ENABLE; // ADC ¿¬¼Ó ¸ğµå ÃR¼ºÈ­
-	AdcHandler.Init.NbrOfConversion = 1; // ADC º¯ÃQ °³¼ö ¼³Á¤
-	AdcHandler.Init.ExternalTrigConv = ADC_SOFTWARE_START; // ADC ¿ÜºÎ Æ®¸®°Å OFF
-	HAL_ADC_Init(&AdcHandler); // ADC¸¦ ¼³Á¤µÈ °ªÀ¸·Î ÃÊ±âÈ­
-	sConfig.Channel = ADC_CHANNEL_4; // ADC Ã¤³Î ¼³Á¤(PA´Â Ã¤³Î 4¹ø)
-	sConfig.Rank = 1; // ADC Ã¤³Î ¼øÀ§ ¼³Á¤
-	sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES; // ADC »ùÇÃ¸µ Å¸ÀÓ ¼³Á¤(3Å¬·°)
-	HAL_ADC_ConfigChannel(&AdcHandler, &sConfig); // Ã¤³Î ¼³Á¤
+	AdcHandler.Init.ScanConvMode = DISABLE; // ADC scan Â¸Ã°ÂµÃ¥ ÂºÃ±ÃƒRÂ¼ÂºÃˆÂ­
+	AdcHandler.Init.ContinuousConvMode = ENABLE; // ADC Â¿Â¬Â¼Ã“ Â¸Ã°ÂµÃ¥ ÃƒRÂ¼ÂºÃˆÂ­
+	AdcHandler.Init.NbrOfConversion = 1; // ADC ÂºÂ¯ÃƒQ Â°Â³Â¼Ã¶ Â¼Â³ÃÂ¤
+	AdcHandler.Init.ExternalTrigConv = ADC_SOFTWARE_START; // ADC Â¿ÃœÂºÃ Ã†Â®Â¸Â®Â°Ã… OFF
+	HAL_ADC_Init(&AdcHandler); // ADCÂ¸Â¦ Â¼Â³ÃÂ¤ÂµÃˆ Â°ÂªÃ€Â¸Â·Ã ÃƒÃŠÂ±Ã¢ÃˆÂ­
+	sConfig.Channel = ADC_CHANNEL_4; // ADC ÃƒÂ¤Â³Ã Â¼Â³ÃÂ¤(PAÂ´Ã‚ ÃƒÂ¤Â³Ã 4Â¹Ã¸)
+	sConfig.Rank = 1; // ADC ÃƒÂ¤Â³Ã Â¼Ã¸Ã€Â§ Â¼Â³ÃÂ¤
+	sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES; // ADC Â»Ã¹Ã‡ÃƒÂ¸Âµ Ã…Â¸Ã€Ã“ Â¼Â³ÃÂ¤(3Ã…Â¬Â·Â°)
+	HAL_ADC_ConfigChannel(&AdcHandler, &sConfig); // ÃƒÂ¤Â³Ã Â¼Â³ÃÂ¤
 	/* NVIC configuration */
 	HAL_NVIC_SetPriority(ADC_IRQn, 0, 0);
 	HAL_NVIC_EnableIRQ(ADC_IRQn);
@@ -133,8 +133,8 @@ main(int argc, char* argv[])
 	LED_Config();
 	/* Configure ADC */
 	ADC_Config();
-	// Interrupt ¹æ½ÄÀ¸·Î ADC º¯ÃQ ¼öÇà
-	HAL_ADC_Start_IT(&AdcHandler); // ADC¸¦ µ¿ÀÛ½ÃÅ´, ŸÚÅÍ·´Æ® ÃR¼ºÈ­
+	// Interrupt Â¹Ã¦Â½Ã„Ã€Â¸Â·Ã ADC ÂºÂ¯ÃƒQ Â¼Ã¶Ã‡Ã 
+	HAL_ADC_Start_IT(&AdcHandler); // ADCÂ¸Â¦ ÂµÂ¿Ã€Ã›Â½ÃƒÃ…Â´, Å¸ÃšÃ…ÃÂ·Â´Ã†Â® ÃƒRÂ¼ÂºÃˆÂ­
   // At this stage the system clock should have already been configured
   // at high speed.
 
@@ -146,11 +146,11 @@ main(int argc, char* argv[])
 
     }
 }
-/*ŸÚÅÍ·´Æ® ·çÆ¾*/
+/*Å¸ÃšÃ…ÃÂ·Â´Ã†Â® Â·Ã§Ã†Â¾*/
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *AdcHandler)
 {
-	  adc_value = HAL_ADC_GetValue(AdcHandler); // ADC º¯ÃQ °á°ú °ªÀ» ÀúÀå
-	  // º¯ÃQ °á°ú°ªÀ» ÀÌ¿ëÇÏ¿© LEDÀÇ ÁÖ±â¸¦ º¯°æ
+	  adc_value = HAL_ADC_GetValue(AdcHandler); // ADC ÂºÂ¯ÃƒQ Â°Ã¡Â°Ãº Â°ÂªÃ€Â» Ã€ÃºÃ€Ã¥
+	  // ÂºÂ¯ÃƒQ Â°Ã¡Â°ÃºÂ°ÂªÃ€Â» Ã€ÃŒÂ¿Ã«Ã‡ÃÂ¿Â© LEDÃ€Ã‡ ÃÃ–Â±Ã¢Â¸Â¦ ÂºÂ¯Â°Ã¦
 	  HAL_GPIO_WritePin(GPIOA,GPIO_PIN_2,1);
 	  HAL_GPIO_WritePin(GPIOA,GPIO_PIN_3,1);
 		  us_delay_int_count(adc_value);
